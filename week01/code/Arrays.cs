1e3a02a1e0891d8803ff242f3b5b001a5f470618
julian.cuervo <julian.cuervo@paxzu.co>
2026-07-04 22:37:15 -0400

    feat: week 1 Dynamics

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,13 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        var result = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = (i + 1) * number;
+        }
+
+        return result;
     }
 
     /// <summary>
@@ -29,5 +37,10 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        var slice1 = data.GetRange(0, data.Count() - amount);
+        data.RemoveRange(0, data.Count() - amount);
+        data.InsertRange(amount, slice1);
+
     }
 }
